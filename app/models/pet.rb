732a3b8d88627d5d1e171ffff65b24868a7947c6
Rodrigo Toledo <rodrigotoledo@192.168.0.102>
2020-09-22 22:43:05 -0300

    add test directory

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -5,4 +5,13 @@ class Pet < ApplicationRecord
   enum pet_type: [ :dog, :cat, :others ]
 
   validates :name, :date_of_birth, :pet_type, presence: true
+  validate :valid_birthday?
+
+  protected
+
+  def valid_birthday?
+    if !date_of_birth.is_a?(Date)
+      errors.add(:date_of_birth, 'must be a valid date') 
+    end
+  end
 end
