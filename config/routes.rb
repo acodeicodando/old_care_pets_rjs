b58f50cb460f7bc989e2284d2fd6bfb10290f255
Rodrigo Toledo <rodrigotoledo@192.168.0.102>
2020-09-23 09:34:56 -0300

    pets api controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    get 'pets/index'
+    get 'pets/create'
+    get 'pets/edit'
+    get 'pets/update'
+    get 'pets/show'
+  end
+  root 'pets#index'
+
+  namespace :api do
+    resources :pets, only: [:index, :create, :edit, :update, :show]
+  end
+
+  get '*path', to: 'pets#omdex', via: :all
 end
